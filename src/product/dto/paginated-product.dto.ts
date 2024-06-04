9c66a745adb08b4aefdc304d50afaa7feae8b3af
Indera-Aji-Waskitho <waskitho@outlook.com>
2024-06-04 09:50:45 +0700

    revert paginated product type to non-generic

--- src/product/dto/paginated-product.dto.ts
+++ src/product/dto/paginated-product.dto.ts
@@ -1,6 +1,21 @@
-import { ObjectType } from '@nestjs/graphql';
-import { Paginated } from 'src/common/paginated';
+import { Field, ObjectType } from '@nestjs/graphql';
 import { Product } from '../entities/product.entity';
+import { PageInfo } from 'src/common/page-info';
 
 @ObjectType()
-export class PaginatedProduct extends Paginated(Product) {}
+export class ProductEdge {
+  @Field(() => String)
+  cursor: string;
+
+  @Field(() => Product)
+  node: Product;
+}
+
+@ObjectType()
+export class PaginatedProduct {
+  @Field(() => [ProductEdge], { nullable: true })
+  edges: ProductEdge[];
+
+  @Field(() => PageInfo, { nullable: true })
+  pageInfo?: PageInfo;
+}
