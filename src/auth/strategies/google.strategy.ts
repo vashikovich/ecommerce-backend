0170f73daf1d337b0d99b4847691ee1de088ba02
Indera-Aji-Waskitho <waskitho@outlook.com>
2024-06-24 07:19:20 +0700

    update env

--- src/auth/strategies/google.strategy.ts
+++ src/auth/strategies/google.strategy.ts
@@ -9,7 +9,7 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
     super({
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: 'http://localhost:3001/auth/google/callback',
+      callbackURL: process.env.API_HOST + '/auth/google/callback',
       scope: ['email', 'profile'],
     });
   }
